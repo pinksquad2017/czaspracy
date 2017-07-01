8c2b9ba68281a22d9c20fb218d0c9ce4258382f5
Katarzyna Jurek <jkatarzy@d17-l427-pc-09.ipa.iisg.agh.edu.pl>
2017-07-01 14:39:30 +0200

    update: option1,option2

--- czaspracy/src/czaspracy/App.java
+++ czaspracy/src/czaspracy/App.java
@@ -1,7 +1,10 @@
 package czaspracy;
 
+import java.util.ArrayList;
+
 public class App {
 	
+	ArrayList<Record> data;
 	
 	public static void main(String[] args){
 		
@@ -14,10 +17,25 @@ public class App {
 	
 	private void start(){
 		
-		DataLoader dataLoader = new DataLoader();
+		DataLoader dataLoader  = new DataLoader();
+		
 		if(dataLoader.getData() != null){
+			System.out.println("Dane zostały poprawnie wczytane.");
+		//	data = dataLoader.getData();
+			data = new ArrayList<Record>();
+			data.add(new Record("Kowalski","analiza","analiza danych","1","12","2016", 2));
+			data.add(new Record("Nowak","analiza","analiza danych","1","12","2016", 3));
+			data.add(new Record("Nowak","analiza","analiza danych","2","12","2016", 3));
+			//test
 			
+		    Menu menu = new Menu(data);
+		    
+		    
+		    
+			menu.start();
+		}else{
 			
+			System.out.println("Dane nie zostały poprawnie wczytane.");
 		}
 				
 	}
