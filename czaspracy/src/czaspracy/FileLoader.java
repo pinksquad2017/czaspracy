021800796ba6a57b1c92aa54cbc6982277c4d4dd
Katarzyna Jurek <jkatarzy@d17-l427-pc-09.ipa.iisg.agh.edu.pl>
2017-07-01 15:22:29 +0200

    fileloader

---
+++ czaspracy/src/czaspracy/FileLoader.java
@@ -0,0 +1,36 @@
+package czaspracy;
+
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileLoader {
+	
+	List<File> files;
+
+	
+	public FileLoader(){
+		files = new ArrayList<>();
+	}
+	public List<File> getFiles(String directoryPath) {
+
+		File folder = new File(directoryPath);
+		File[] listOfFiles = folder.listFiles();
+		
+	
+		for(File file : listOfFiles) {
+	
+			if(file.isFile()){
+				files.add(file);
+			}
+			else if (file.isDirectory()){
+				getFiles(file.getAbsolutePath());
+			}
+			files.add(file);
+			System.out.println("Dodaje do listy plik " + file.getName());
+		}
+		return files;
+	}
+
+}
